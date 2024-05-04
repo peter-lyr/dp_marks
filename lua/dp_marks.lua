7d1477975017464a0853b1934da11ab086d24f60
peter-lyr <llydrp.ldp@gmail.com>
2024-05-05 01:40:51 +0800

    M.has_marks()

--- lua/dp_marks.lua
+++ lua/dp_marks.lua
@@ -12,17 +12,41 @@ end
 
 function M.temp_map_jump_mark()
   B.temp_map {
-    { 'k', function() M.prev() end, mode = { 'n', 'v', }, silent = true, desc = 'marks: prev', },
-    { 'j', function() M.next() end, mode = { 'n', 'v', }, silent = true, desc = 'marks: next', },
+    { 'k', function() require 'marks'.prev() end, mode = { 'n', 'v', }, silent = true, desc = 'marks: prev', },
+    { 'j', function() require 'marks'.next() end, mode = { 'n', 'v', }, silent = true, desc = 'marks: next', },
   }
 end
 
+function M.has_marks()
+  local marks = {}
+  for _, data in ipairs(vim.fn.getmarklist '%') do
+    local mark = data.mark:sub(2, 3)
+    if string.match(mark, '%w') then
+      marks[#marks + 1] = mark
+    end
+  end
+  if #marks > 0 then
+    table.insert(marks, 1, string.format('%d marks', #marks))
+    B.set_timeout(100, function()
+      B.notify_info_append(marks, 1000 * 60 * 60 * 24)
+    end)
+    return 1
+  end
+  print 'not a-z marks'
+end
+
 function M.next()
+  if not M.has_marks() then
+    return
+  end
   M.temp_map_jump_mark()
   require 'marks'.next()
 end
 
 function M.prev()
+  if not M.has_marks() then
+    return
+  end
   M.temp_map_jump_mark()
   require 'marks'.prev()
 end
